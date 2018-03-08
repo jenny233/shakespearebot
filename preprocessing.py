3a6e300c0cbf19efb3bf9adcb727f73fce622928
Jenny Li <jieni.li@yahoo.com>
2018-03-08 01:59:44 -0800

    add code to make sure all words in sonnets match the dictionary

--- preprocessing.py
+++ preprocessing.py
@@ -9,6 +9,17 @@ X = pp.load_poems("data/shakespeare.txt")
 
 SYMBOL_LIST = {'?', '.',  ')', ',', '!', ';', '(', ':'}
 
+word_list = []
+with open("data/Syllable_dictionary.txt") as f:
+    while True:
+        line = f.readline()
+        if line == "":
+            break
+        line = line.strip()
+        lst = line.split()
+        word_list.append(lst[0])
+print("word_list:", len(word_list))
+
 def load_poems(filename, onepoem=False):
     '''
     filename should be directory/filename.
@@ -39,13 +50,13 @@ def load_poems(filename, onepoem=False):
             line = line.lower()
             # Split into an array of words
             x = line.split(" ")
+            # Check if it is in the Syllable_dictionary
+            for i in range(len(x)):
+                if not x[i] in word_list:
+                    x[i] = x[i].replace("\'", "")
             # Add to the matrix of lines
             X.append(x)
     return X
 
-def set_of_words(X):
-    word_set = set()
-    for x in X:
-        for word in x:
-            word_set.add(word)
-    return word_set
+def list_of_words(X):
+    return word_list
