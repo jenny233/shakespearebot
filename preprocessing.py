0e0ceeca4ca5458a33950702eec645800228ae4b
Jenny Li <jieni.li@yahoo.com>
2018-03-08 01:35:27 -0800

    add parameter onepoem in case if you want only one poem

--- preprocessing.py
+++ preprocessing.py
@@ -8,9 +8,10 @@ X = pp.load_poems("data/shakespeare.txt")
 '''
 
 SYMBOL_LIST = {'?', '.',  ')', ',', '!', ';', '(', ':'}
-def load_poems(filename):
+def load_poems(filename, onepoem=False):
     '''
-    filename should be directory/filename
+    filename should be directory/filename.
+    if onepoem, then only return data for the 1st poem.
     Returns the matrix X, where each row is a line of a poem, and each col is
             a word of the line.
     '''
@@ -23,6 +24,10 @@ def load_poems(filename):
                 break
             # Remove whitespaces from the front and back
             line = line.strip()
+
+            if onepoem and line == "2":
+                break
+
             # If it is not an actual poem line
             if len(line) < 5:
                 continue
